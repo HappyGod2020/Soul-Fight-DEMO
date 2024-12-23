2f574637dfa0b2753626b6f99af047ef46e7790c
MuratBG-KGU <muratovbogdan4@gmail.com>
2024-12-23 17:58:58 +0500

    Добавление ходьбы по горизонтали. Улучшено реагирование на нажатие пробела. Улучшена подгрузка уровня.

--- GUI/GameScreen.py
+++ GUI/GameScreen.py
@@ -2,6 +2,7 @@ from GUI.BaseScreen import BaseScreen
 from model.Player import Player
 from core.settings import GUI_SETTINGS
 import pygame
+import csv
 
 
 class Platform(pygame.sprite.Sprite):
@@ -12,25 +13,23 @@ class Platform(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=(x, y))
 
 
-
-
 class GameScreen(BaseScreen):
-
     def init(self):
         self.player = Player(50, GUI_SETTINGS.HEIGHT - 100, 50, 50, "player_sprite.png")
         self.platforms = pygame.sprite.Group()
-        self.create_map()
+        self.load_map_from_csv("level1.csv")  # Загрузка карты
         self.add_event(self.handle_input)
 
-    def create_map(self):
-        platform_positions = [
-            (0, GUI_SETTINGS.HEIGHT - 50, 100, 20),  # Нижний блок
-            (200, GUI_SETTINGS.HEIGHT - 150, 100, 20),  # Средний блок
-            (400, GUI_SETTINGS.HEIGHT - 250, 100, 20),  # Верхний блок
-        ]
-        for x, y, w, h in platform_positions:
-            platform = Platform(x, y, w, h, "tile.png")
-            self.platforms.add(platform)
+    def load_map_from_csv(self, filepath):
+        """Загрузка карты из CSV файла"""
+        tile_size = 50  # Размер одной плитки
+        with open(filepath, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for y, row in enumerate(reader):
+                for x, cell in enumerate(row):
+                    if cell == "1":
+                        platform = Platform(x * tile_size, y * tile_size, tile_size, tile_size, "tile.png")
+                        self.platforms.add(platform)
 
     def render(self):
         self.screen.fill((135, 206, 250))  # Светло-голубой фон
@@ -38,6 +37,15 @@ class GameScreen(BaseScreen):
         self.screen.blit(self.player.image, self.player.rect)
         self.player.update(self.platforms)
 
-    def handle_input(self, e):
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
-            self.player.jump()
+    def handle_input(self, event):
+        """Обработка событий ввода"""
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:  # Кнопка "A"
+                self.player.move_left()
+            elif event.key == pygame.K_d:  # Кнопка "D"
+                self.player.move_right()
+            elif event.key == pygame.K_SPACE:  # Прыжок
+                self.player.jump()
+        elif event.type == pygame.KEYUP:
+            if event.key in [pygame.K_a, pygame.K_d]:  # Остановка при отпускании "A" или "D"
+                self.player.stop()
--- model/Player.py
+++ model/Player.py
@@ -1,29 +1,63 @@
 import pygame
 
-
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, sprite_path):
+    def __init__(self, x, y, width, height, sprite_path="player_sprite.png"):
         super().__init__()
         self.image = pygame.image.load(sprite_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (width, height))
         self.rect = self.image.get_rect(topleft=(x, y))
-
-        self.velocity_y = 0
-        self.gravity = 0.1
-        self.jump_speed = -5
-        self.grounded = False
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.speed = 3  # Скорость движения по горизонтали
+        self.gravity = 0.2
+        self.jump_strength = -15
+        self.on_ground = False
 
     def update(self, platforms):
-        self.velocity_y += self.gravity
-        self.rect.y += self.velocity_y
+        """Обновление состояния игрока"""
+        self.apply_gravity()
+        self.move_and_collide(platforms)
 
-        self.grounded = False
+    def apply_gravity(self):
+        """Применение гравитации"""
+        if not self.on_ground:
+            self.velocity.y += self.gravity
+
+    def move_and_collide(self, platforms):
+        """Перемещение с учётом столкновений"""
+        # Горизонтальное движение
+        self.rect.x += self.velocity.x
         for platform in platforms:
-            if self.rect.colliderect(platform.rect) and self.velocity_y > 0:
-                self.rect.bottom = platform.rect.top
-                self.velocity_y = 0
-                self.grounded = True
+            if self.rect.colliderect(platform.rect):
+                if self.velocity.x > 0:  # Движение вправо
+                    self.rect.right = platform.rect.left
+                elif self.velocity.x < 0:  # Движение влево
+                    self.rect.left = platform.rect.right
+
+        # Вертикальное движение
+        self.rect.y += self.velocity.y
+        self.on_ground = False  # Сбрасываем флаг нахождения на земле
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.velocity.y > 0:  # Падение вниз
+                    self.rect.bottom = platform.rect.top
+                    self.velocity.y = 0
+                    self.on_ground = True
+                elif self.velocity.y < 0:  # Удар о потолок
+                    self.rect.top = platform.rect.bottom
+                    self.velocity.y = 0
 
     def jump(self):
-        if self.grounded:
-            self.velocity_y = self.jump_speed
+        """Прыжок"""
+        self.velocity.y = self.jump_strength
+
+    def move_left(self):
+        """Движение влево"""
+        self.velocity.x = -self.speed
+
+    def move_right(self):
+        """Движение вправо"""
+        self.velocity.x = self.speed
+
+    def stop(self):
+        """Остановка"""
+        self.velocity.x = 0
