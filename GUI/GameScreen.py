8c54c86d0b3cf219f197e84449771096a99e01b1
slawa <slawamihailov125@yandex.ru>
2025-01-06 17:45:24 +0300

    Сделан прыжок и частично готово горизонтальное движение

--- GUI/GameScreen.py
+++ GUI/GameScreen.py
@@ -9,7 +9,7 @@ from core.settings import GUI_SETTINGS
 
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, sprite_path="Sprite/tile.png"):
+    def __init__(self, x, y, width, height, sprite_path="Sprite/блок1.2.png"):
         super().__init__()
         self.image = pygame.image.load(sprite_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (width, height))
@@ -23,9 +23,10 @@ class GameScreen(BaseScreen):
         self.levels_folder = "levels"
         self.level_index = 1
         self.door = None
-        self.player = Player(x=50, y=400, width=GUI_SETTINGS.HEIGHT // 16, height=GUI_SETTINGS.HEIGHT // 16)  # Начальная позиция игрока
+        self.player = Player(x=48, y=400, width=GUI_SETTINGS.HEIGHT // 16, height=GUI_SETTINGS.HEIGHT // 16)  # Начальная позиция игрока
         self.load_level()
         self.add_event(self.handle_input)
+        self.clock = pygame.time.Clock()
 
     def load_level(self):
         """Загрузка текущего уровня из CSV."""
@@ -43,16 +44,16 @@ class GameScreen(BaseScreen):
             reader = csv.reader(csvfile)
             for y, row in enumerate(reader):
                 for x, cell in enumerate(row):
-                    x_pos = x * 50
-                    y_pos = y * 50
+                    x_pos = x * 48
+                    y_pos = y * 48
                     if cell == "1":  # Платформа
-                        platform = Platform(x * 50, y * 50, 50, 50)  # Размеры платформ
+                        platform = Platform(x * 48, y * 48, 48, 48)  # Размеры платформ
                         self.platforms.append(platform)
                     elif cell == "2":  # Шипы
-                        spike = Spike(x * 50, y * 50, 50, 50)
+                        spike = Spike(x * 48, y * 48, 48, 48)
                         self.spikes.append(spike)
                     elif cell == "3":  # Дверь
-                        self.door = Door(x * 50, y * 50, 50, 50)
+                        self.door = Door(x * 48, y * 48, 48, 48)
     #
     # def load_map(self):
     #     """Загрузка карты из CSV файла."""
@@ -68,7 +69,11 @@ class GameScreen(BaseScreen):
     #                     self.spikes.append(spike)
     #                 elif tile == "3":  # Дверь
     #                     self.door = Door(x * 50, y * 50, 50, 50)
+
     def render(self):
+        # self.image = pygame.image.load('Sprite/страшный фон.png').convert_alpha()
+        # self.image = pygame.transform.scale(self.image, (1920, 1080))
+        # self.screen.blit(self.image, (0, 0))
         """Отрисовка игрового процесса."""
         # Отрисовка платформ
         for platform in self.platforms:
@@ -111,11 +116,11 @@ class GameScreen(BaseScreen):
         """Обработка ввода с клавиатуры."""
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                self.player.move_left()
+                self.player.update(GUI_SETTINGS.WIDTH, GUI_SETTINGS.HEIGHT, self.platforms, 1)
             elif event.key == pygame.K_d:
-                self.player.move_right()
+                self.player.update(GUI_SETTINGS.WIDTH, GUI_SETTINGS.HEIGHT, self.platforms, 2)
             elif event.key == pygame.K_SPACE:
                 self.player.jump()
-        elif event.type == pygame.KEYUP:
+        if event.type == pygame.KEYUP:
             if event.key in [pygame.K_a, pygame.K_d]:
-                self.player.stop()
+                self.player.update(GUI_SETTINGS.WIDTH, GUI_SETTINGS.HEIGHT, self.platforms, flag_stop=True)
--- model/Player.py
+++ model/Player.py
@@ -4,15 +4,24 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, sprite_path="Sprite/player_sprite.png"):
         super().__init__()
+        self.move_flag = 0
+        self.flag_stop = False
         self.image = pygame.image.load(sprite_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (width, height))
         self.rect = self.image.get_rect(topleft=(x, y))
         self.velocity = pygame.math.Vector2(0, 0)
-        self.speed = 2  # Скорость движения по горизонтали
-        self.gravity = 0.5
-        self.max_fall_speed = 2  # Максимальная скорость падения
-        self.jump_strength = -10
+        self.max_speed_x = 0.7  # Скорость движения по горизонтали
+        self.new_speed_x = 0
+        self.new_speed_y = 0
+        self.player_boost = 30
+        self.spop_boost = 15
+        self.speed = 5
+        self.g = 30
+        # self.max_fall_speed = 2  # Максимальная скорость падения
+        self.jump_speed = 15
         self.on_ground = False
+        self.clock = pygame.time.Clock()
+        self.fps = 80
 
     def respawn(self, x, y):
         """Респавн игрока в начальной позиции."""
@@ -32,6 +41,7 @@ class Player(pygame.sprite.Sprite):
                 elif self.velocity.x < 0:  # Движение влево
                     self.rect.left = platform.rect.right
 
+
         # Ограничение по горизонтали
         if self.rect.left < 0:  # Левая граница
             self.rect.left = 0
@@ -45,11 +55,12 @@ class Player(pygame.sprite.Sprite):
         # Проверка столкновений по вертикали
         for platform in platforms:
             if self.rect.colliderect(platform.rect):
-                if self.velocity.y > 0:  # Падение вниз
+                if self.new_speed_y >= 0:  # Падение вниз
                     self.rect.bottom = platform.rect.top
                     self.velocity.y = 0
+                    self.new_speed_y = 0
                     self.on_ground = True
-                elif self.velocity.y < 0:  # Удар о потолок
+                elif self.new_speed_y <= 0:  # Удар о потолок
                     self.rect.top = platform.rect.bottom
                     self.velocity.y = 0
 
@@ -57,35 +68,81 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top < 0:  # Верхняя граница
             self.rect.top = 0
             self.velocity.y = 0
-        if self.rect.bottom > screen_height:  # Нижняя граница
+            self.new_speed_y = 0
+        if self.rect.bottom >= screen_height:  # Нижняя граница
             self.rect.bottom = screen_height
+            self.new_speed_y = 0
             self.on_ground = True
 
-    def update(self, screen_width, screen_height, platforms):
+    def update(self, screen_width, screen_height, platforms, move_flag=0, flag_stop=False):
         """Обновление состояния игрока"""
-        self.apply_gravity()
+        if move_flag:
+            self.move_flag = move_flag
+        if flag_stop:
+            self.flag_stop = flag_stop
+            self.move_flag = 0
+        self.clock.tick(self.fps)
         self.move(screen_width, screen_height, platforms)
+        if self.new_speed_y < 0 or (not self.on_ground):
+            self.apply_gravity()
+        if self.move_flag == 2 and not flag_stop:
+            self.flag_stop = False
+            self.move_right()
+        if self.move_flag == 1 and not flag_stop:
+            self.flag_stop = False
+            self.move_left()
+        if self.flag_stop:
+            self.stop()
+            if self.new_speed_x == 0:
+                self.flag_stop = False
 
     def apply_gravity(self):
         """Применение гравитации"""
-
-        self.velocity.y += self.gravity
-        if self.velocity.y > self.max_fall_speed:
-            self.velocity.y = self.max_fall_speed
+        self.velocity.y += self.new_speed_y * 0.0125 + (self.g * 0.0125 ** 2) / 2
+        self.new_speed_y += self.g * 0.0125
 
     def jump(self):
         """Прыжок"""
-
-        self.velocity.y = self.jump_strength
+        self.new_speed_y = -self.jump_speed
 
     def move_left(self):
         """Движение влево"""
-        self.velocity.x = -self.speed
+        if abs(self.new_speed_x - self.player_boost * 0.0125) <= self.max_speed_x:
+            self.velocity.x += self.new_speed_x * 0.0125 - (self.player_boost * 0.0125 ** 2) / 2
+            if abs(self.new_speed_x) < self.max_speed_x / 2:
+                self.new_speed_x -= self.player_boost * 10 * 0.0125
+            else:
+                self.new_speed_x -= self.player_boost * 5 * 0.0125
+        else:
+            self.velocity.x += self.new_speed_x * 0.0125
 
     def move_right(self):
         """Движение вправо"""
-        self.velocity.x = self.speed
+        print(self.new_speed_x)
+        if self.new_speed_x + self.player_boost * 0.0125 <= self.max_speed_x:
+            self.velocity.x += self.new_speed_x * 0.0125 + (self.player_boost * 0.0125 ** 2) / 2
+            self.new_speed_x += self.player_boost * 0.0125
+            if self.new_speed_x < self.max_speed_x / 2:
+                self.new_speed_x += self.player_boost * 10 * 0.0125
+            else:
+                self.new_speed_x -= self.player_boost * 5 * 0.0125
+        else:
+            self.velocity.x += self.new_speed_x * 0.0125
 
     def stop(self):
         """Остановка"""
-        self.velocity.x = 0
+        if self.new_speed_x != 0:
+            if self.new_speed_x > 0:
+                if self.new_speed_x - self.spop_boost * 0.0125 <= 0:
+                    self.new_speed_x = 0
+                    self.velocity.x = 0
+                else:
+                    self.velocity.x += self.new_speed_x * 0.0125 - (self.spop_boost * 0.0125 ** 2) / 2
+                    self.new_speed_x -= self.spop_boost * 0.0125
+            if self.new_speed_x < 0:
+                if self.new_speed_x + self.spop_boost * 0.0125 >= 0:
+                    self.new_speed_x = 0
+                    self.velocity.x = 0
+                else:
+                    self.velocity.x += self.new_speed_x * 0.0125 + (self.spop_boost * 0.0125 ** 2) / 2
+                    self.new_speed_x += self.spop_boost * 0.0125
